dde33dfb20184d72e1e83d3c8f2e3e8a9b381259
Douglas Wade Goodall <douglas_goodall@mac.com>
2025-08-13 10:28:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "// install.cpp 2025-08-13 10:14 dwg - "     > install.cpp
-echo "#include <stdio>"                          >> install.cpp
+echo "#include <stdio.h>"                          >> install.cpp
 echo "int main() {"                               >> install.cpp
 echo "  std::cout << __PRETTY_FUNCTION__"        >> install.cpp
 echo "  std::cout << std::endl"                  >> install.cpp
