1da2c6ff8fae646b9a5d8db1ccbd12625c12feae
Douglas Wade Goodall <douglas_goodall@mac.com>
2025-08-13 11:11:07 -0700

    Create program.sh

---
+++ program.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "// install.cpp 2025-08-13 10:14 dwg - "     > install.cpp
+echo "#include <stdio.h>"                          >> install.cpp
+echo "int main() {"                               >> install.cpp
+echo "  printf(\"%s\n\",__PRETTY_FUNCTION__);"    >> install.cpp  
+echo "}"                                         >> install.cpp
+gcc install.cpp -o install
+./install
+#rm install.cpp install
